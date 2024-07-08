101e6a44bf5706efc28cc09a130336d052348f94
PabloProfessional <pablo.muigai@outlook.com>
2024-07-08 22:45:44 +0300

    Dashboard - WIP | Total Budget and Spend DONE

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -65,7 +65,7 @@ onMounted(async () => {
                 <div
                     class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#c45d25] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#c45d25]"
                 >
-                    <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch" style="width: 80%;">
+                    <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch" style="width: 100%;">
                         <div class="map-container ">
 
 
@@ -92,7 +92,7 @@ onMounted(async () => {
                         <div class="pt-12 sm:pt-12 lg:pt-0">
                             <h2 class="text-xl font-semibold text-black dark:text-white" style="color: #343c54">Country Map</h2>
 
-                            <p class="mt-4 text-sm/relaxed">
+                            <p class="mt-2 text-sm/relaxed">
                                 The chart on the left shows a summary analysis of key measurements.
                             </p>
 
