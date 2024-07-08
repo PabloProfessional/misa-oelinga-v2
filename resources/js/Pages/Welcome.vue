3b0b720be33bc1c023b4eae46c18b29087b33f53
PabloProfessional <pablo.muigai@outlook.com>
2024-07-08 13:05:36 +0300

    Welcome Page - DONE

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -17,7 +17,7 @@ function handleImageError() {
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="MISA" />
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
 <!--        <img-->
 <!--            id="background"-->
@@ -25,13 +25,13 @@ function handleImageError() {
 <!--            src="https://laravel.com/assets/img/welcome/background.svg"-->
 <!--        />-->
         <div
-            class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
+            class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#c45d25] selection:text-white"
         >
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                 <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                     <div class="flex lg:justify-center lg:col-start-2">
 <!--                        <svg-->
-<!--                            class="h-12 w-auto text-white lg:h-16 lg:text-[#FF2D20]"-->
+<!--                            class="h-12 w-auto text-white lg:h-16 lg:text-[#c45d25]"-->
 <!--                            viewBox="0 0 62 65"-->
 <!--                            fill="none"-->
 <!--                            xmlns="http://www.w3.org/2000/svg"-->
@@ -47,7 +47,7 @@ function handleImageError() {
                         <Link
                             v-if="$page.props.auth.user"
                             :href="route('dashboard')"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#c45d25] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
                             Dashboard
                         </Link>
@@ -55,7 +55,7 @@ function handleImageError() {
 <!--                        <template v-else>-->
 <!--                            <Link-->
 <!--                                :href="route('login')"-->
-<!--                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"-->
+<!--                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#c45d25] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"-->
 <!--                            >-->
 <!--                                Log in-->
 <!--                            </Link>-->
@@ -63,7 +63,7 @@ function handleImageError() {
 <!--                            <Link-->
 <!--                                v-if="canRegister"-->
 <!--                                :href="route('register')"-->
-<!--                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"-->
+<!--                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#c45d25] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"-->
 <!--                            >-->
 <!--                                Register-->
 <!--                            </Link>-->
@@ -74,9 +74,9 @@ function handleImageError() {
                 <main class="mt-6">
                     <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
                         <a
-                            href="https://laravel.com/docs"
+                            href="#"
                             id="docs-card"
-                            class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
+                            class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#c45d25] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#c45d25]"
                         >
                             <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
                                 <img
@@ -98,7 +98,7 @@ function handleImageError() {
                             <div class="relative flex items-center gap-6 lg:items-end">
                                 <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
                                     <div
-                                        class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16"
+                                        class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#c45d25]/10 sm:size-16"
                                     >
                                         <svg
                                             class="size-5 sm:size-6"
@@ -107,11 +107,11 @@ function handleImageError() {
                                             viewBox="0 0 24 24"
                                         >
                                             <path
-                                                fill="#FF2D20"
+                                                fill="#c45d25"
                                                 d="M23 4a1 1 0 0 0-1.447-.894L12.224 7.77a.5.5 0 0 1-.448 0L2.447 3.106A1 1 0 0 0 1 4v13.382a1.99 1.99 0 0 0 1.105 1.79l9.448 4.728c.14.065.293.1.447.1.154-.005.306-.04.447-.105l9.453-4.724a1.99 1.99 0 0 0 1.1-1.789V4ZM3 6.023a.25.25 0 0 1 .362-.223l7.5 3.75a.251.251 0 0 1 .138.223v11.2a.25.25 0 0 1-.362.224l-7.5-3.75a.25.25 0 0 1-.138-.22V6.023Zm18 11.2a.25.25 0 0 1-.138.224l-7.5 3.75a.249.249 0 0 1-.329-.099.249.249 0 0 1-.033-.12V9.772a.251.251 0 0 1 .138-.224l7.5-3.75a.25.25 0 0 1 .362.224v11.2Z"
                                             />
                                             <path
-                                                fill="#FF2D20"
+                                                fill="#c45d25"
                                                 d="m3.55 1.893 8 4.048a1.008 1.008 0 0 0 .9 0l8-4.048a1 1 0 0 0-.9-1.785l-7.322 3.706a.506.506 0 0 1-.452 0L4.454.108a1 1 0 0 0-.9 1.785H3.55Z"
                                             />
                                         </svg>
@@ -128,28 +128,28 @@ function handleImageError() {
                                     </div>
                                 </div>
 
-                                <svg
-                                    class="size-6 shrink-0 stroke-[#FF2D20]"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-                                    />
-                                </svg>
+<!--                                <svg-->
+<!--                                    class="size-6 shrink-0 stroke-[#c45d25]"-->
+<!--                                    xmlns="http://www.w3.org/2000/svg"-->
+<!--                                    fill="none"-->
+<!--                                    viewBox="0 0 24 24"-->
+<!--                                    stroke-width="1.5"-->
+<!--                                >-->
+<!--                                    <path-->
+<!--                                        stroke-linecap="round"-->
+<!--                                        stroke-linejoin="round"-->
+<!--                                        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"-->
+<!--                                    />-->
+<!--                                </svg>-->
                             </div>
                         </a>
 
                         <a
                             href="/login"
-                            class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
+                            class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#c45d25] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#c45d25]"
                         >
                             <div
-                                class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16"
+                                class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#c45d25]/10 sm:size-16"
                             >
                                 <svg
                                     class="size-5 sm:size-6"
@@ -157,7 +157,7 @@ function handleImageError() {
                                     fill="none"
                                     viewBox="0 0 24 24"
                                 >
-                                    <g fill="#FF2D20">
+                                    <g fill="#c45d25">
                                         <path
                                             d="M24 8.25a.5.5 0 0 0-.5-.5H.5a.5.5 0 0 0-.5.5v12a2.5 2.5 0 0 0 2.5 2.5h19a2.5 2.5 0 0 0 2.5-2.5v-12Zm-7.765 5.868a1.221 1.221 0 0 1 0 2.264l-6.626 2.776A1.153 1.153 0 0 1 8 18.123v-5.746a1.151 1.151 0 0 1 1.609-1.035l6.626 2.776ZM19.564 1.677a.25.25 0 0 0-.177-.427H15.6a.106.106 0 0 0-.072.03l-4.54 4.543a.25.25 0 0 0 .177.427h3.783c.027 0 .054-.01.073-.03l4.543-4.543ZM22.071 1.318a.047.047 0 0 0-.045.013l-4.492 4.492a.249.249 0 0 0 .038.385.25.25 0 0 0 .14.042h5.784a.5.5 0 0 0 .5-.5v-2a2.5 2.5 0 0 0-1.925-2.432ZM13.014 1.677a.25.25 0 0 0-.178-.427H9.101a.106.106 0 0 0-.073.03l-4.54 4.543a.25.25 0 0 0 .177.427H8.4a.106.106 0 0 0 .073-.03l4.54-4.543ZM6.513 1.677a.25.25 0 0 0-.177-.427H2.5A2.5 2.5 0 0 0 0 3.75v2a.5.5 0 0 0 .5.5h1.4a.106.106 0 0 0 .073-.03l4.54-4.543Z"
                                         />
@@ -174,7 +174,7 @@ function handleImageError() {
                             </div>
 
                             <svg
-                                class="size-6 shrink-0 self-center stroke-[#FF2D20]"
+                                class="size-6 shrink-0 self-center stroke-[#c45d25]"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
                                 viewBox="0 0 24 24"
