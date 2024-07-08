90272709ec7b2a80645d4965d4e6e970362f3018
PabloProfessional <pablo.muigai@outlook.com>
2024-07-08 22:32:09 +0300

    Dashboard - WIP | Total Budget and Spend DONE

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,6 +6,7 @@ import {computed, onMounted, ref,} from "vue";
 import axios from "axios";
 import Budget from "@/Components/Budget.vue";
 import Spend from "@/Components/Spend.vue";
+import AssetHealthPieChart from "@/Pages/Partials/AssetHealthPieChart.vue";
 const count = ref<number>(0);
 const budget = ref<number>(0);
 const spend = ref<number>(0);
@@ -57,7 +58,54 @@ onMounted(async () => {
                     <Spend :spend="spend" :variance="variance"></Spend>
                 </div>
             </div>
+            <br>
+            <br>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-6 space-y-6">
+
+                <div
+                    class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#c45d25] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#c45d25]"
+                >
+                    <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch" style="width: 80%;">
+                        <div class="map-container ">
+
+
+                            <img src="../../../public/img/maps/za_map.png" alt="South Africa Map" style="width: 100%;">
+
+<!--                            @foreach ($provinces as $province)-->
+
+<!--                            <div class="province-label {{ $province->url }}" onclick="location.href='province/{{ $province->url }}';">-->
+<!--                                <div><strong>{{ $province->name }}</strong></div>-->
+<!--                                <div>@if ($province->status() !== null)-->
+<!--                                    <strong>-->
+<!--                                        <i class=" {{ $province->status()->icon }}" style="color: {{ $province->status()->color }}"></i>-->
+<!--                                        <span style="color: {{ $province->status()->color }}">{{ $province->status()->name }}</span>-->
+<!--                                    </strong>-->
+<!--                                    @else-->
+<!--                                    <span class="badge bg-warning">-->
+<!--                                            No projects yet.-->
+<!--                                             </span>-->
+<!--                                    @endif</div>-->
+<!--                            </div>-->
+
+<!--                            @endforeach-->
+                        </div>
+                        <div class="pt-12 sm:pt-12 lg:pt-0">
+                            <h2 class="text-xl font-semibold text-black dark:text-white" style="color: #343c54">Country Map</h2>
+
+                            <p class="mt-4 text-sm/relaxed">
+                                The chart on the left shows a summary analysis of key measurements.
+                            </p>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
         </div>
+
+
 
     </AuthenticatedLayout>
 </template>
---
+++ resources/js/Pages/Partials/AssetHealthPieChart.vue
@@ -0,0 +1,40 @@
+<script>
+import { defineComponent } from 'vue';
+import VueApexCharts from 'vue3-apexcharts';
+
+export default defineComponent({
+    components: {
+        apexchart: VueApexCharts
+    }
+});
+</script>
+
+<script setup>
+import { ref } from 'vue';
+
+// Define reactive data
+const series = ref([3,1,0,0,0,0]);
+
+const chartOptions = ref({
+    chart: {
+        type: 'donut',
+    },
+
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: {
+                width: 500
+            },
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }]
+});
+</script>
+
+<template>
+
+        <apexchart type="donut" height="250" :options="chartOptions" :series="series"></apexchart>
+</template>
