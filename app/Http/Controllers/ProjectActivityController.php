b88a0295f80d1ed0fa340ced7ad331b75f76218e
PabloProfessional <pablo.muigai@outlook.com>
2024-07-15 10:33:40 +0300

    Project Activity Create - DONE

--- app/Http/Controllers/ProjectActivityController.php
+++ app/Http/Controllers/ProjectActivityController.php
@@ -50,48 +50,52 @@ class ProjectActivityController extends Controller
      */
     public function store(StoreProjectActivityRequest $request)
     {
-        //
-        dd($request->all());
+        //dd($request->all());
 
-        $project = Project::where('uuid',$request->project_uuid)->first();
+        $project = Project::where('url',$request->project_url)->first();
 
 
-        $data = $request->validate([
+        $request->validate([
             'name' => 'required|string|max:255',
             'project_activity_type' => 'required|numeric',
             'description' => 'nullable|string',
             //'location' => 'required|numeric',
             'project_stage' => 'required|numeric',
-            'budget' => 'nullable|numeric',
-            'spend' => 'nullable|numeric',
+            'budget' => 'nullable|string',
+            'spend' => 'nullable|string',
             'start_date' => 'required|date',
             'end_date' => 'required|date|after_or_equal:start_date',
             'manager' => 'required|numeric',
-            'priority_id' => 'required|numeric',
+            //'priority_id' => 'required|numeric',
             'attachments.*' => 'nullable|file',
             'notes' => 'nullable|string',
         ]);
+
+        $statuses = [$request->procurement_status,$request->budget_status,$request->schedule_status,$request->risk_status];
+        $status = array_sum($statuses) / count($statuses);
+        $budget = (int) str_replace('R ','',str_replace(',','',$request->budget)) * 100;
+        $spend = (int) str_replace('R ','',str_replace(',','',$request->spend)) * 100;
 
         $projectActivity = ProjectActivity::create([
             'project_id' => $project->id,
             'user_id' => auth()->user()->id,
             'name' => $request->name,
-            'activity_type_id' => $request->activity_type,
+            'activity_type_id' => $request->project_activity_type,
             'description' => $request->description,
             'location' => $project->municipal_id,
-            'stage_type_id' =>  $request->project_stage,
-            'budget' => $request->budget,
-            'spend' => $request->spend,
+            'budget' => $budget,
+            'spend' => $spend,
             'start_date' => $request->start_date,
             'end_date' => $request->end_date,
             'manager_id' => $request->manager,
-            'priority_id' => $request->priotity_id,
+            'priority_id' => 1,
             'team_members' => json_encode($request->team_members),
             'attachments' => json_encode($request->attachments),
             'notes' => $request->note,
             'stakeholder_id' => $request->manager,
-            'status_type_id' => $request->activity_status,
-            'url' => Str::slug($request->name),
+            'stage_type_id' =>  $request->project_stage,
+            'status_type_id' => (int)$status,
+            'url' => Str::slug($request->name).time(),
             'color' => sprintf("#%02x%02x%02x", rand(0, 255), rand(0, 255), rand(0, 255))
         ]);
 
@@ -101,9 +105,9 @@ class ProjectActivityController extends Controller
         ProjectAccount::create([
             'project_id' => $project->id,
             'credit' => 0,
-            'debit' => $request->spend,
+            'debit' => $spend,
             'description' => $projectActivity->name.' - '.$projectActivity->description,
-            'balance' => $project->project_accounts->first()->balance - $request->spend
+            'balance' => $project->project_accounts->first()->balance - $spend
         ]);
 
         return redirect('/project/'.$project->url)->with('status','Project Activity: '.$projectActivity->name.' added to project');
--- app/Models/ProjectActivity.php
+++ app/Models/ProjectActivity.php
@@ -8,4 +8,44 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectActivity extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'project_id',
+        'activity_type_id',
+        'user_id',
+        'manager_id',
+        'stakeholder_id',
+        'location',
+        'stage_type_id',
+        'status_type_id',
+        'budget',
+        'spend',
+        'description',
+        'url',
+        'color',
+        'start_date',
+        'end_date',
+        'team_members',
+        'attachments',
+        'notes',
+        'priority_id',
+        'tags',
+    ];
+
+    public function activity_type(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(ProjectActivityType::class,'activity_type_id');
+    }
+
+    public function status_type(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(StatusType::class,'status_type_id');
+    }
+
+    public function project(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Project::class,'project_id');
+    }
+
 }
